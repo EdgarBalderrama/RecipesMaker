c1e428b1ac5bf1a6a41ba0db98a103580819985b
Edgar Ruben Balderrama <edgarbalderrama0821@gmail.com>
2020-03-30 22:08:39 -0400

    Agregando el punto 2 y agregando comentarios

--- src/RecipesMaker.kt
+++ src/RecipesMaker.kt
@@ -1,27 +1,23 @@
-import java.lang.IndexOutOfBoundsException
 import java.lang.NumberFormatException
-import java.text.NumberFormat
 
-val listaIngredientes : List<String> =
-    listOf("Agua", "Leche", "Carne", "Verduras", "Frutas", "Cereal", "Huevos", "Aceite")
-
-
+// Ejecución del programa mediante el menú principal
 fun main() {
-
     do {
         showMenu()
         val resultadoTeclado : Int?  = readLine()?.toInt()
-        when(resultadoTeclado)
-        {
-            1 -> hacerReceta()
-            3 -> println("Hasta pronto!")
-        }
+            when(resultadoTeclado) {
+                1 -> hacerReceta()
+                2 -> mostrarReceta()
+                3 -> println("Hasta pronto!")
+            }
+        println()
     } while (resultadoTeclado != 3)
-
 }
 
-fun showMenu()
-{
+/**
+ * Despliega el menú de opciones
+ * */
+fun showMenu() {
     println("Selecciona la opción deseada:")
     println("1. Hacer una receta")
     println("2. Ver mis recetas")
@@ -30,8 +26,21 @@ fun showMenu()
     println()
 }
 
-fun hacerReceta()
-{
+// Lista de ingredientes seleccionados por el usuario
+val listaIngredientesSeleccionados : MutableList<String> = mutableListOf()
+
+// Cuenta la cantidad de ingredientes que ya fueron ingresados por el usuario
+var contadorLista : Int = 0
+
+
+/**
+ * Despliega los ingredientes del menú de listaIngredientes y permite al usuario elegir uno. Posteriormente lo añade a otra lista.
+ *
+ * */
+fun hacerReceta() {
+    val listaIngredientes : List<String> =
+        listOf("Agua", "Leche", "Carne", "Zanahoria", "Tomate", "Cebolla", "Manzana", "Naranja", "Platano", "Cereal", "Huevo", "Aceite")
+
     println("A continuación se muestran los ingredientes disponibles: ")
     var contador = 1
     for (ingredientes in listaIngredientes)
@@ -46,6 +55,9 @@ fun hacerReceta()
         println("Elegiste:" )
         println(listaIngredientes[ingredienteSeleccionado!!-1])
 
+        listaIngredientesSeleccionados?.add(contadorLista, listaIngredientes[ingredienteSeleccionado-1])
+        contador.inc()
+
     }catch (e: NullPointerException)
     {
         println("No se permiten valores null")
@@ -55,10 +67,25 @@ fun hacerReceta()
     }catch (e: NumberFormatException)
     {
         println("Verifique el valor que desea ingresar")
-
     }
+    println()
+}
 
 
-    println("Se volverá al menú principal:")
-
+/**
+ * Despliega los ingredientes seleccionados por el usuario
+ * */
+fun mostrarReceta(){
+    println("Los ingredientes que selecciono hasta el momento son: ")
+    var contador = 1
+    if (listaIngredientesSeleccionados != null) {
+        for (ingredientes in listaIngredientesSeleccionados) {
+            println("$contador: $ingredientes")
+            contador++
+        }
+    }
+    else {
+        println("Aun no selecciono ningun ingrediente")
+    }
+    println()
 }
